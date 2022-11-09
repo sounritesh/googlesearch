2a1d67ea7f444ad9b85eddf3112e93f856432a40
Ritesh Soun <sounritesh@gmail.com>
2022-11-09 14:12:15 +0530

    chore: add logs

--- googlesearch/__init__.py
+++ googlesearch/__init__.py
@@ -19,7 +19,7 @@ def _req(term, results, lang, start, proxies):
         ),
         proxies=proxies,
     )
-    print(f"RESP: {resp}")
+    print(f"RESP: {resp.json()}")
     resp.raise_for_status()
     return resp
 
