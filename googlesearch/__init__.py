ae7446bca91035deedf12808c225c20986dbcbab
Ritesh Soun <sounritesh@gmail.com>
2023-01-23 12:48:50 +0530

    feat: sleep and retry on failure

--- googlesearch/__init__.py
+++ googlesearch/__init__.py
@@ -1,24 +1,32 @@
 from bs4 import BeautifulSoup
 from requests import get
+from time import sleep
 
 usr_agent = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
 
 
 def _req(term, session, results, lang, start, proxies):
-    resp = session.get(
-        url="https://www.google.com/search",
-        headers=usr_agent,
-        params=dict(
-            q=term,
-            num=results + 2,  # Prevents multiple requests
-            hl=lang,
-            start=start,
-        ),
-        proxies=proxies,
-    )
-    resp.raise_for_status()
-    return resp
+    for i in range(5):
+        try:
+            resp = session.get(
+                url="https://www.google.com/search",
+                headers=usr_agent,
+                params=dict(
+                    q=term,
+                    num=results + 2,  # Prevents multiple requests
+                    hl=lang,
+                    start=start,
+                ),
+                proxies=proxies,
+            )
+            resp.raise_for_status()
+            return resp
+        except Exception as e:
+            print(f"ERROR: {e}\n Sleeping for 2 seconds.")
+            sleep(2)
+            continue
+    return None
 
 
 class SearchResult:
@@ -51,6 +59,24 @@ def search(term, session, num_results=10, lang="en", proxy=None, advanced=False)
                     start, lang, start, proxies)
 
         # Parse
+        if not resp:
+            try:
+                resp = get(
+                    url="https://www.google.com/search",
+                    headers=usr_agent,
+                    params=dict(
+                        q=term,
+                        num=num_results - start + 2,  # Prevents multiple requests
+                        hl=lang,
+                        start=start,
+                    ),
+                    proxies=proxies,
+                )
+                resp.raise_for_status()
+            except Exception as e:
+                print(f"ERROR: {e}")
+                break
+
         soup = BeautifulSoup(resp.text, 'html.parser')
         result_block = soup.find_all('div', attrs={'class': 'g'})
 
